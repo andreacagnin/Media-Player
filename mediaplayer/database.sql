b7b390b68283004ba25d75113db6f2ddf42d4ebd
Andrea Cagnin <147907225+andreacagnin@users.noreply.github.com>
2024-03-28 16:53:45 +0100

    Create database.sql

---
+++ mediaplayer/database.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mar 28, 2024 alle 16:50
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mediaplayer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `film`
+--
+
+CREATE TABLE `film` (
+  `id_film` int(11) NOT NULL,
+  `descrizione` text DEFAULT NULL,
+  `durata` time DEFAULT NULL,
+  `data_produzione` date DEFAULT NULL,
+  `paese_produzione` varchar(100) DEFAULT NULL,
+  `id_regista` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `regista`
+--
+
+CREATE TABLE `regista` (
+  `id_regista` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `cognome` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id_film`),
+  ADD KEY `produzione` (`id_regista`);
+
+--
+-- Indici per le tabelle `regista`
+--
+ALTER TABLE `regista`
+  ADD PRIMARY KEY (`id_regista`);
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `film`
+--
+ALTER TABLE `film`
+  ADD CONSTRAINT `produzione` FOREIGN KEY (`id_regista`) REFERENCES `regista` (`id_regista`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
