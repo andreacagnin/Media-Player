127e2fff77ce9070cfa884a3c2c197a5c9f2b25e
damianogobbo <cagnin.st.andrea@maxplanck.edu.it>
2024-04-04 09:38:35 +0200

    Update mediaplayer.sql

--- database/mediaplayer.sql
+++ database/mediaplayer.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 03, 2024 alle 14:05
--- Versione del server: 10.4.27-MariaDB
--- Versione PHP: 8.2.0
+-- Creato il: Apr 04, 2024 alle 09:37
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,13 +38,6 @@ CREATE TABLE `film` (
   `id_regista` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dump dei dati per la tabella `film`
---
-
-INSERT INTO `film` (`id_film`, `titolo`, `copertina`, `descrizione`, `durata`, `data_produzione`, `paese_produzione`, `id_regista`) VALUES
-(1, 'mao', 'pepepepepepep', 'maopao', '14:04:24', '2024-04-03', 'zulu', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -56,13 +49,6 @@ CREATE TABLE `regista` (
   `nome` varchar(100) DEFAULT NULL,
   `cognome` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `regista`
---
-
-INSERT INTO `regista` (`id_regista`, `nome`, `cognome`) VALUES
-(1, 'damiano', 'gobbo');
 
 --
 -- Indici per le tabelle scaricate
