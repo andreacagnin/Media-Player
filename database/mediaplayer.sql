33a0b7f52e581dcccad3879524c9e4d363c9234a
Andrea Cagnin <147907225+andreacagnin@users.noreply.github.com>
2024-04-15 07:35:10 +0100

    Database (#8)
    * Create database.sql
    
    * Updated database
    
    * Create .gitignore
    
    * path renamed
    
    * Update mediaplayer.sql
    
    * Update .gitignore
    
    * Update mediaplayer.sql
    
    * Update .gitignore
    
    * Update mediaplayer.sql
    
    ---------
    
    Co-authored-by: AymenKabine2003 <147838962+AymenKabine2003@users.noreply.github.com>

---
+++ database/mediaplayer.sql
@@ -0,0 +1,385 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 14, 2024 alle 17:04
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mediaplayer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `country`
+--
+
+CREATE TABLE `country` (
+  `CountryID` int(11) NOT NULL,
+  `CountryName` varchar(50) DEFAULT NULL,
+  `TwoCharCountryCode` char(2) DEFAULT NULL,
+  `ThreeCharCountryCode` char(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `country`
+--
+
+INSERT INTO `country` (`CountryID`, `CountryName`, `TwoCharCountryCode`, `ThreeCharCountryCode`) VALUES
+(1, 'Afghanistan', 'AF', 'AFG'),
+(2, 'Aland Islands', 'AX', 'ALA'),
+(3, 'Albania', 'AL', 'ALB'),
+(4, 'Algeria', 'DZ', 'DZA'),
+(5, 'American Samoa', 'AS', 'ASM'),
+(6, 'Andorra', 'AD', 'AND'),
+(7, 'Angola', 'AO', 'AGO'),
+(8, 'Anguilla', 'AI', 'AIA'),
+(9, 'Antarctica', 'AQ', 'ATA'),
+(10, 'Antigua and Barbuda', 'AG', 'ATG'),
+(11, 'Argentina', 'AR', 'ARG'),
+(12, 'Armenia', 'AM', 'ARM'),
+(13, 'Aruba', 'AW', 'ABW'),
+(14, 'Australia', 'AU', 'AUS'),
+(15, 'Austria', 'AT', 'AUT'),
+(16, 'Azerbaijan', 'AZ', 'AZE'),
+(17, 'Bahamas', 'BS', 'BHS'),
+(18, 'Bahrain', 'BH', 'BHR'),
+(19, 'Bangladesh', 'BD', 'BGD'),
+(20, 'Barbados', 'BB', 'BRB'),
+(21, 'Belarus', 'BY', 'BLR'),
+(22, 'Belgium', 'BE', 'BEL'),
+(23, 'Belize', 'BZ', 'BLZ'),
+(24, 'Benin', 'BJ', 'BEN'),
+(25, 'Bermuda', 'BM', 'BMU'),
+(26, 'Bhutan', 'BT', 'BTN'),
+(27, 'Bolivia', 'BO', 'BOL'),
+(28, 'Bonaire, Sint Eustatius and Saba', 'BQ', 'BES'),
+(29, 'Bosnia and Herzegovina', 'BA', 'BIH'),
+(30, 'Botswana', 'BW', 'BWA'),
+(31, 'Bouvet Island', 'BV', 'BVT'),
+(32, 'Brazil', 'BR', 'BRA'),
+(33, 'British Indian Ocean Territory', 'IO', 'IOT'),
+(34, 'Brunei', 'BN', 'BRN'),
+(35, 'Bulgaria', 'BG', 'BGR'),
+(36, 'Burkina Faso', 'BF', 'BFA'),
+(37, 'Burundi', 'BI', 'BDI'),
+(38, 'Cambodia', 'KH', 'KHM'),
+(39, 'Cameroon', 'CM', 'CMR'),
+(40, 'Canada', 'CA', 'CAN'),
+(41, 'Cape Verde', 'CV', 'CPV'),
+(42, 'Cayman Islands', 'KY', 'CYM'),
+(43, 'Central African Republic', 'CF', 'CAF'),
+(44, 'Chad', 'TD', 'TCD'),
+(45, 'Chile', 'CL', 'CHL'),
+(46, 'China', 'CN', 'CHN'),
+(47, 'Christmas Island', 'CX', 'CXR'),
+(48, 'Cocos (Keeling) Islands', 'CC', 'CCK'),
+(49, 'Colombia', 'CO', 'COL'),
+(50, 'Comoros', 'KM', 'COM'),
+(51, 'Congo', 'CG', 'COG'),
+(52, 'Cook Islands', 'CK', 'COK'),
+(53, 'Costa Rica', 'CR', 'CRI'),
+(54, 'Ivory Coast', 'CI', 'CIV'),
+(55, 'Croatia', 'HR', 'HRV'),
+(56, 'Cuba', 'CU', 'CUB'),
+(57, 'Curacao', 'CW', 'CUW'),
+(58, 'Cyprus', 'CY', 'CYP'),
+(59, 'Czech Republic', 'CZ', 'CZE'),
+(60, 'Democratic Republic of the Congo', 'CD', 'COD'),
+(61, 'Denmark', 'DK', 'DNK'),
+(62, 'Djibouti', 'DJ', 'DJI'),
+(63, 'Dominica', 'DM', 'DMA'),
+(64, 'Dominican Republic', 'DO', 'DOM'),
+(65, 'Ecuador', 'EC', 'ECU'),
+(66, 'Egypt', 'EG', 'EGY'),
+(67, 'El Salvador', 'SV', 'SLV'),
+(68, 'Equatorial Guinea', 'GQ', 'GNQ'),
+(69, 'Eritrea', 'ER', 'ERI'),
+(70, 'Estonia', 'EE', 'EST'),
+(71, 'Ethiopia', 'ET', 'ETH'),
+(72, 'Falkland Islands (Malvinas)', 'FK', 'FLK'),
+(73, 'Faroe Islands', 'FO', 'FRO'),
+(74, 'Fiji', 'FJ', 'FJI'),
+(75, 'Finland', 'FI', 'FIN'),
+(76, 'France', 'FR', 'FRA'),
+(77, 'French Guiana', 'GF', 'GUF'),
+(78, 'French Polynesia', 'PF', 'PYF'),
+(79, 'French Southern Territories', 'TF', 'ATF'),
+(80, 'Gabon', 'GA', 'GAB'),
+(81, 'Gambia', 'GM', 'GMB'),
+(82, 'Georgia', 'GE', 'GEO'),
+(83, 'Germany', 'DE', 'DEU'),
+(84, 'Ghana', 'GH', 'GHA'),
+(85, 'Gibraltar', 'GI', 'GIB'),
+(86, 'Greece', 'GR', 'GRC'),
+(87, 'Greenland', 'GL', 'GRL'),
+(88, 'Grenada', 'GD', 'GRD'),
+(89, 'Guadaloupe', 'GP', 'GLP'),
+(90, 'Guam', 'GU', 'GUM'),
+(91, 'Guatemala', 'GT', 'GTM'),
+(92, 'Guernsey', 'GG', 'GGY'),
+(93, 'Guinea', 'GN', 'GIN'),
+(94, 'Guinea-Bissau', 'GW', 'GNB'),
+(95, 'Guyana', 'GY', 'GUY'),
+(96, 'Haiti', 'HT', 'HTI'),
+(97, 'Heard Island and McDonald Islands', 'HM', 'HMD'),
+(98, 'Honduras', 'HN', 'HND'),
+(99, 'Hong Kong', 'HK', 'HKG'),
+(100, 'Hungary', 'HU', 'HUN'),
+(101, 'Iceland', 'IS', 'ISL'),
+(102, 'India', 'IN', 'IND'),
+(103, 'Indonesia', 'ID', 'IDN'),
+(104, 'Iran', 'IR', 'IRN'),
+(105, 'Iraq', 'IQ', 'IRQ'),
+(106, 'Ireland', 'IE', 'IRL'),
+(107, 'Isle of Man', 'IM', 'IMN'),
+(108, 'Israel', 'IL', 'ISR'),
+(109, 'Italy', 'IT', 'ITA'),
+(110, 'Jamaica', 'JM', 'JAM'),
+(111, 'Japan', 'JP', 'JPN'),
+(112, 'Jersey', 'JE', 'JEY'),
+(113, 'Jordan', 'JO', 'JOR'),
+(114, 'Kazakhstan', 'KZ', 'KAZ'),
+(115, 'Kenya', 'KE', 'KEN'),
+(116, 'Kiribati', 'KI', 'KIR'),
+(117, 'Kosovo', 'XK', '---'),
+(118, 'Kuwait', 'KW', 'KWT'),
+(119, 'Kyrgyzstan', 'KG', 'KGZ'),
+(120, 'Laos', 'LA', 'LAO'),
+(121, 'Latvia', 'LV', 'LVA'),
+(122, 'Lebanon', 'LB', 'LBN'),
+(123, 'Lesotho', 'LS', 'LSO'),
+(124, 'Liberia', 'LR', 'LBR'),
+(125, 'Libya', 'LY', 'LBY'),
+(126, 'Liechtenstein', 'LI', 'LIE'),
+(127, 'Lithuania', 'LT', 'LTU'),
+(128, 'Luxembourg', 'LU', 'LUX'),
+(129, 'Macao', 'MO', 'MAC'),
+(130, 'Macedonia', 'MK', 'MKD'),
+(131, 'Madagascar', 'MG', 'MDG'),
+(132, 'Malawi', 'MW', 'MWI'),
+(133, 'Malaysia', 'MY', 'MYS'),
+(134, 'Maldives', 'MV', 'MDV'),
+(135, 'Mali', 'ML', 'MLI'),
+(136, 'Malta', 'MT', 'MLT'),
+(137, 'Marshall Islands', 'MH', 'MHL'),
+(138, 'Martinique', 'MQ', 'MTQ'),
+(139, 'Mauritania', 'MR', 'MRT'),
+(140, 'Mauritius', 'MU', 'MUS'),
+(141, 'Mayotte', 'YT', 'MYT'),
+(142, 'Mexico', 'MX', 'MEX'),
+(143, 'Micronesia', 'FM', 'FSM'),
+(144, 'Moldava', 'MD', 'MDA'),
+(145, 'Monaco', 'MC', 'MCO'),
+(146, 'Mongolia', 'MN', 'MNG'),
+(147, 'Montenegro', 'ME', 'MNE'),
+(148, 'Montserrat', 'MS', 'MSR'),
+(149, 'Morocco', 'MA', 'MAR'),
+(150, 'Mozambique', 'MZ', 'MOZ'),
+(151, 'Myanmar (Burma)', 'MM', 'MMR'),
+(152, 'Namibia', 'NA', 'NAM'),
+(153, 'Nauru', 'NR', 'NRU'),
+(154, 'Nepal', 'NP', 'NPL'),
+(155, 'Netherlands', 'NL', 'NLD'),
+(156, 'New Caledonia', 'NC', 'NCL'),
+(157, 'New Zealand', 'NZ', 'NZL'),
+(158, 'Nicaragua', 'NI', 'NIC'),
+(159, 'Niger', 'NE', 'NER'),
+(160, 'Nigeria', 'NG', 'NGA'),
+(161, 'Niue', 'NU', 'NIU'),
+(162, 'Norfolk Island', 'NF', 'NFK'),
+(163, 'North Korea', 'KP', 'PRK'),
+(164, 'Northern Mariana Islands', 'MP', 'MNP'),
+(165, 'Norway', 'NO', 'NOR'),
+(166, 'Oman', 'OM', 'OMN'),
+(167, 'Pakistan', 'PK', 'PAK'),
+(168, 'Palau', 'PW', 'PLW'),
+(169, 'Palestine', 'PS', 'PSE'),
+(170, 'Panama', 'PA', 'PAN'),
+(171, 'Papua New Guinea', 'PG', 'PNG'),
+(172, 'Paraguay', 'PY', 'PRY'),
+(173, 'Peru', 'PE', 'PER'),
+(174, 'Phillipines', 'PH', 'PHL'),
+(175, 'Pitcairn', 'PN', 'PCN'),
+(176, 'Poland', 'PL', 'POL'),
+(177, 'Portugal', 'PT', 'PRT'),
+(178, 'Puerto Rico', 'PR', 'PRI'),
+(179, 'Qatar', 'QA', 'QAT'),
+(180, 'Reunion', 'RE', 'REU'),
+(181, 'Romania', 'RO', 'ROU'),
+(182, 'Russia', 'RU', 'RUS'),
+(183, 'Rwanda', 'RW', 'RWA'),
+(184, 'Saint Barthelemy', 'BL', 'BLM'),
+(185, 'Saint Helena', 'SH', 'SHN'),
+(186, 'Saint Kitts and Nevis', 'KN', 'KNA'),
+(187, 'Saint Lucia', 'LC', 'LCA'),
+(188, 'Saint Martin', 'MF', 'MAF'),
+(189, 'Saint Pierre and Miquelon', 'PM', 'SPM'),
+(190, 'Saint Vincent and the Grenadines', 'VC', 'VCT'),
+(191, 'Samoa', 'WS', 'WSM'),
+(192, 'San Marino', 'SM', 'SMR'),
+(193, 'Sao Tome and Principe', 'ST', 'STP'),
+(194, 'Saudi Arabia', 'SA', 'SAU'),
+(195, 'Senegal', 'SN', 'SEN'),
+(196, 'Serbia', 'RS', 'SRB'),
+(197, 'Seychelles', 'SC', 'SYC'),
+(198, 'Sierra Leone', 'SL', 'SLE'),
+(199, 'Singapore', 'SG', 'SGP'),
+(200, 'Sint Maarten', 'SX', 'SXM'),
+(201, 'Slovakia', 'SK', 'SVK'),
+(202, 'Slovenia', 'SI', 'SVN'),
+(203, 'Solomon Islands', 'SB', 'SLB'),
+(204, 'Somalia', 'SO', 'SOM'),
+(205, 'South Africa', 'ZA', 'ZAF'),
+(206, 'South Georgia and the South Sandwich Islands', 'GS', 'SGS'),
+(207, 'South Korea', 'KR', 'KOR'),
+(208, 'South Sudan', 'SS', 'SSD'),
+(209, 'Spain', 'ES', 'ESP'),
+(210, 'Sri Lanka', 'LK', 'LKA'),
+(211, 'Sudan', 'SD', 'SDN'),
+(212, 'Suriname', 'SR', 'SUR'),
+(213, 'Svalbard and Jan Mayen', 'SJ', 'SJM'),
+(214, 'Swaziland', 'SZ', 'SWZ'),
+(215, 'Sweden', 'SE', 'SWE'),
+(216, 'Switzerland', 'CH', 'CHE'),
+(217, 'Syria', 'SY', 'SYR'),
+(218, 'Taiwan', 'TW', 'TWN'),
+(219, 'Tajikistan', 'TJ', 'TJK'),
+(220, 'Tanzania', 'TZ', 'TZA'),
+(221, 'Thailand', 'TH', 'THA'),
+(222, 'Timor-Leste (East Timor)', 'TL', 'TLS'),
+(223, 'Togo', 'TG', 'TGO'),
+(224, 'Tokelau', 'TK', 'TKL'),
+(225, 'Tonga', 'TO', 'TON'),
+(226, 'Trinidad and Tobago', 'TT', 'TTO'),
+(227, 'Tunisia', 'TN', 'TUN'),
+(228, 'Turkey', 'TR', 'TUR'),
+(229, 'Turkmenistan', 'TM', 'TKM'),
+(230, 'Turks and Caicos Islands', 'TC', 'TCA'),
+(231, 'Tuvalu', 'TV', 'TUV'),
+(232, 'Uganda', 'UG', 'UGA'),
+(233, 'Ukraine', 'UA', 'UKR'),
+(234, 'United Arab Emirates', 'AE', 'ARE'),
+(235, 'United Kingdom', 'GB', 'GBR'),
+(236, 'United States', 'US', 'USA'),
+(237, 'United States Minor Outlying Islands', 'UM', 'UMI'),
+(238, 'Uruguay', 'UY', 'URY'),
+(239, 'Uzbekistan', 'UZ', 'UZB'),
+(240, 'Vanuatu', 'VU', 'VUT'),
+(241, 'Vatican City', 'VA', 'VAT'),
+(242, 'Venezuela', 'VE', 'VEN'),
+(243, 'Vietnam', 'VN', 'VNM'),
+(244, 'Virgin Islands, British', 'VG', 'VGB'),
+(245, 'Virgin Islands, US', 'VI', 'VIR'),
+(246, 'Wallis and Futuna', 'WF', 'WLF'),
+(247, 'Western Sahara', 'EH', 'ESH'),
+(248, 'Yemen', 'YE', 'YEM'),
+(249, 'Zambia', 'ZM', 'ZMB'),
+(250, 'Zimbabwe', 'ZW', 'ZWE');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `film`
+--
+
+CREATE TABLE `film` (
+  `id_film` int(11) NOT NULL,
+  `film_path` text NOT NULL,
+  `titolo` varchar(100) NOT NULL,
+  `copertina` text NOT NULL,
+  `descrizione` text DEFAULT NULL,
+  `durata` time DEFAULT NULL,
+  `data_produzione` date DEFAULT NULL,
+  `paese_produzione` int(11) DEFAULT NULL,
+  `id_regista` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `film`
+--
+
+INSERT INTO `film` (`id_film`, `film_path`, `titolo`, `copertina`, `descrizione`, `durata`, `data_produzione`, `paese_produzione`, `id_regista`) VALUES
+(1, 'https://www.youtube.com/embed/fYgME5_PAzQ', 'Be Good', 'media\\copertine\\be_good.jpg', 'Paul - a workaholic independent filmmaker - takes charge of his sleepless baby, Pearl, when his wife, Mary, returns to work. He is slowly driven crazy by his inability to work and becomes the jerk he thought he\'d never be.', '01:10:00', '2012-06-22', 236, 1),
+(2, 'https://www.youtube.com/embed/VEMF5NBjAY8', 'Il Buono Il Brutto Il Cattivo', 'media\\copertine\\il_buono_il_brutto_il_cattivo.jpg', 'Ambientato durante la guerra di secessione americana, nella metà dell’Ottocento, il film ha per protagonista tre uomini senza scrupoli, ognuno con le proprie regole morali che li collocano ai margini della società e della legge. Si tratta di Joe, detto il buono, Tuco, detto il brutto, e Sentenza, detto il cattivo. I primi due sono soliti collaborare inscenando alcune truffe, salvo poi tradirsi a vicenda. Le loro strade finiranno però per rincrociarsi lungo un percorso che porta ad un ricco tesoro nascosto. Alla ricerca di questo vi è però anche il temibile Sentenza. Ben presto i tre finiranno per scontrarsi all’ultimo sangue, mentre sullo sfondo l’America cambia per sempre.', '02:54:23', '1966-12-23', 109, 2),
+(3, 'https://www.youtube.com/embed/KOFu-sEvwuI', 'Downfall', 'media\\copertine\\downfall.jpg', 'Berlino, aprile 1945. Gli ultimi giorni della vita di Adolf Hitler nel bunker sotto la Cancelleria, dove il dittatore si era rifugiato insieme agli altri capi del regime nazista mentre l\'esercito russo si accingeva a conquistare la capitale tedesca.', '02:35:56', '2005-04-29', 83, 3),
+(4, 'https://www.dailymotion.com/embed/video/x8l6nmn?autoplay=1', 'Tre uomini e una gamba', 'media\\copertine\\tre_uomini_e_una_gamba.jpg', 'Un viaggio in auto dal nord al sud del Paese, in occasione di un matrimonio, si trasforma in un\'odissea epica per tre impiegati e una preziosa gamba di legno.', '01:37:51', '1997-12-27', 109, 4),
+(5, 'https://www.youtube.com/embed/UoZkulgtVLM', 'La haine', 'media\\copertine\\la_haine.jpg', 'Il racconto di una giornata nella periferia parigina dopo l\'avvenuto pestaggio durante un interrogatorio ai danni di Abdel Ichah attraverso gli occhi di Vinz, Said e Hubert.', '01:37:55', '1995-05-31', 76, 5),
+(6, 'https://www.youtube.com/embed/Q3hBLv-HLEc', 'Stalker', 'media\\copertine\\stalker.jpg', 'Based on the novel \"Roadside Picnic\" by Arkady and Boris Strugatsky. The Zone that arose on Earth for unknown reasons attracts attention with inexplicable phenomena that occur there. A rumor has spread that in the center of the Zone there is something that gives a person everything he wants. But staying in the Zone is deadly, and therefore it is strictly guarded. There, each for their own reasons, the Writer and the Professor go, the Stalker leads them to the mysterious center, feeling and understanding the Zone...', '02:41:45', '1979-05-25', 182, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `regista`
+--
+
+CREATE TABLE `regista` (
+  `id_regista` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `cognome` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `regista`
+--
+
+INSERT INTO `regista` (`id_regista`, `nome`, `cognome`) VALUES
+(1, 'Todd', 'Looby'),
+(2, 'Sergio', 'Leone'),
+(3, 'Oliver', 'Hirschbiegel'),
+(4, 'Massimo', 'Venier'),
+(5, 'Mathieu', 'Kassovitz'),
+(6, 'Andrej', 'Tarkovskij');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`CountryID`);
+
+--
+-- Indici per le tabelle `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id_film`),
+  ADD KEY `produzione` (`id_regista`),
+  ADD KEY `country` (`paese_produzione`);
+
+--
+-- Indici per le tabelle `regista`
+--
+ALTER TABLE `regista`
+  ADD PRIMARY KEY (`id_regista`);
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `film`
+--
+ALTER TABLE `film`
+  ADD CONSTRAINT `country` FOREIGN KEY (`paese_produzione`) REFERENCES `country` (`CountryID`),
+  ADD CONSTRAINT `produzione` FOREIGN KEY (`id_regista`) REFERENCES `regista` (`id_regista`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
